b22c9803e6fc59091e60aeaa2b2df4fed000e8fa
Gabriel Guesser <gdguesser@gmail.com>
2023-03-18 12:15:26 +0000

    latest changes

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type CEPInfo struct {
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	UF          string `json:"uf"`
+}
 
 func main() {
 	someValue := SomeValue()
@@ -9,12 +22,19 @@ func main() {
 
 	multiplication := Multiply(5, -6)
 
+	value, err := GetCEPInfo("01001000")
+	if err != nil {
+		log.Println("test: ", err)
+	}
+
+	fmt.Println(value)
+
 	fmt.Printf("Some value: %s, Addition: %d, Multiplication: %d\n",
 		someValue, addition, multiplication)
 }
 
 func SomeValue() string {
-	return "not expected value"
+	return "expected value"
 }
 
 func Add(a, b int) int {
@@ -23,4 +43,19 @@ func Add(a, b int) int {
 
 func Multiply(a, b int) int {
 	return a * b
+}
+
+func GetCEPInfo(cep string) (*CEPInfo, error) {
+	resp, err := http.Get(fmt.Sprintf("https://viacep.com.br/ws/%s/json/", cep))
+	if err != nil {
+		return nil, fmt.Errorf("failed to perform GET request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	var info CEPInfo
+	if err := json.NewDecoder(resp.Body).Decode(&info); err != nil {
+		return nil, fmt.Errorf("failed to decode response body: %w", err)
+	}
+
+	return &info, nil
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSomeValue(t *testing.T) {
 	result := SomeValue()
@@ -31,5 +34,60 @@ func TestMultiply(t *testing.T) {
 		if result != test.esperado {
 			t.Errorf("Multiply(%d, %d) = %d; want %d", test.a, test.b, result, test.esperado)
 		}
+	}
+}
+
+func TestGetCEPInfo(t *testing.T) {
+	// Define a slice of test cases
+	tests := []struct {
+		cep        string
+		expected   *CEPInfo
+		expectFail bool
+	}{
+		{
+			// First test case
+			cep: "01001000",
+			expected: &CEPInfo{
+				Logradouro:  "Praça da Sé",
+				Complemento: "lado ímpar",
+				Bairro:      "Sé",
+				Localidade:  "São Paulo",
+				UF:          "SP",
+			},
+			expectFail: false, // We expect this test case to succeed
+		},
+		{
+			// Second test case
+			cep:        "invalid_cep",
+			expected:   nil,
+			expectFail: true, // We expect this test case to fail
+		},
+	}
+
+	// Iterate over the test cases using a for loop
+	for _, tc := range tests {
+		// Use t.Run to create a sub-test for each test case
+		t.Run(tc.cep, func(t *testing.T) {
+			// Call the function being tested with the current test case's cep field
+			result, err := GetCEPInfo(tc.cep)
+
+			// If expectFail is true, we expect an error to be returned
+			if tc.expectFail {
+				if err == nil {
+					t.Fatalf("expected an error but got nil")
+				}
+				return
+			}
+
+			// If we expect the function to succeed, but it returns an error, fail the test
+			if err != nil {
+				t.Fatalf("unexpected error: %s", err)
+			}
+
+			// Compare the function's return value to the expected value using reflect.DeepEqual
+			if !reflect.DeepEqual(result, tc.expected) {
+				t.Fatalf("expected %+v, but got %+v", tc.expected, result)
+			}
+		})
 	}
 }
