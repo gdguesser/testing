8a0b7200458970d8883137f3344352c96430a6a7
Gabriel Guesser <gdguesser@gmail.com>
2023-03-18 12:36:39 +0000

    rollback

--- main.go
+++ main.go
@@ -1,19 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
-	"net/http"
 )
-
-type CEPInfo struct {
-	Logradouro  string `json:"logradouro"`
-	Complemento string `json:"complemento"`
-	Bairro      string `json:"bairro"`
-	Localidade  string `json:"localidade"`
-	UF          string `json:"uf"`
-}
 
 func main() {
 	someValue := SomeValue()
@@ -21,13 +10,6 @@ func main() {
 	addition := Add(3, 2)
 
 	multiplication := Multiply(5, -6)
-
-	value, err := GetCEPInfo("01001000")
-	if err != nil {
-		log.Println("test: ", err)
-	}
-
-	fmt.Println(value)
 
 	fmt.Printf("Some value: %s, Addition: %d, Multiplication: %d\n",
 		someValue, addition, multiplication)
@@ -43,19 +25,4 @@ func Add(a, b int) int {
 
 func Multiply(a, b int) int {
 	return a * b
-}
-
-func GetCEPInfo(cep string) (*CEPInfo, error) {
-	resp, err := http.Get(fmt.Sprintf("https://viacep.com.br/ws/%s/json/", cep))
-	if err != nil {
-		return nil, fmt.Errorf("failed to perform GET request: %w", err)
-	}
-	defer resp.Body.Close()
-
-	var info CEPInfo
-	if err := json.NewDecoder(resp.Body).Decode(&info); err != nil {
-		return nil, fmt.Errorf("failed to decode response body: %w", err)
-	}
-
-	return &info, nil
 }
